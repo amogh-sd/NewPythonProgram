23fb65685fd2728be4d9aca8573e8e14a0ea082e
amogh-sd <68118792+amogh-sd@users.noreply.github.com>
2020-07-15 16:53:21 +0530

    Add files via upload

---
+++ amogh.py
@@ -0,0 +1,20 @@
+﻿print ()
+name = input('Please enter your name here  ')
+print ("Hello",(name))
+print ('How do you do')
+input ("")
+print ("Oh great. I'm fine")
+print ()
+print ()
+print ("..................")
+print ("....i have no more progams on me")
+print ()
+print ("bye bye")
+print ()
+print ()
+print ()
+input ("press enter to end")
+    
+    
+
+
